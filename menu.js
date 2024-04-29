33bfa7333c979925cb6134df7a4f96c12fe1d613
Jonny Savastano <jonathan.savastano1246@gmail.com>
2024-04-29 16:14:18 -0600

    fixed menu.js

---
+++ menu.js
@@ -0,0 +1,5 @@
+// Hamburger Menu script
+document.querySelector('.hamburger-button').addEventListener('click', function() {
+    document.querySelector('.nav-links').style.display = 
+    (document.querySelector('.nav-links').style.display == 'none') ? 'block' : 'none';
+});
